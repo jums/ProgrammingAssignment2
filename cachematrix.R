12ac59f153d3dd26d9e3eaede7ea5382872d7c6a
Juhani Markkula <juhani@logic.fi>
2015-06-22 00:28:00 +0300

    Seems legit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Tools to create matrix objects with ability to
+## cache the inverse value and reuse the value.
 
-## Write a short comment describing this function
+## Creates the special matrix object with cached inversion property.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(matrix) {
+    x <<- matrix
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(get = get,
+       set = set,
+       getInverse = getInverse,
+       setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
+## Returns a (cacheMatrix) matrix that is the inverse of 'x'.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+
+  if (!is.null(inv)) {
+    message("Returning cached value.")
+    return(inv)
+  }
+
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  message("Returning calculated value, now cached.")
+  inv
 }
+
